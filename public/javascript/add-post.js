2089e663aa88cff6ed4353a024089fb250d20503
AyalaStonic <ayaladuvan876@gmail.com>
2022-10-05 21:59:32 -0400

    fix JS error

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -1,34 +1,25 @@
-const addPostBtn = document.querySelector('#add-post-btn');
-const createPostEl = document.querySelector('#create-post-card');
-
-function toggleHide(event) {
-    createPostEl.classList.remove('hide');
-    addPostBtn.classList.add('hide');
-};
-
 async function newFormHandler(event) {
     event.preventDefault();
-
+  
     const title = document.querySelector('input[name="post-title"]').value;
-    const content = document.querySelector('textarea[name="post-content"]').value;
-
+    const post_text = document.querySelector('textarea[name="post_text"]').value;
+  
     const response = await fetch(`/api/posts`, {
-        method: 'POST',
-        body: JSON.stringify({
-            title,
-            content
-        }),
-        headers: { 'Content-Type': 'application/json' }
+      method: 'POST',
+      body: JSON.stringify({
+        title,
+        post_text
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
     });
-
+  
     if (response.ok) {
-        addPostBtn.classList.remove('hide');
-        createPostEl.classList.add('hide');
-        document.location.replace('/dashboard');
+      document.location.replace('/dashboard');
     } else {
-        alert(response.statusText);
-    };
-};
-
-document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
-addPostBtn.addEventListener('click', toggleHide)
+      alert(response.statusText);
+    }
+  }
+  
+  document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -1,27 +1,28 @@
 async function editFormHandler(event) {
     event.preventDefault();
-
-    const id = window.location.toString().split('/')[
-        window.location.toString().split('/').length - 1
-    ];
-
+  
     const title = document.querySelector('input[name="post-title"]').value;
-    const content = document.querySelector('textarea[name="post-content"]').value;
-
+    const post_text = document.querySelector('textarea[name="post_text"]').value.trim();
+    
+    const id = window.location.toString().split('/')[
+      window.location.toString().split('/').length - 1
+    ];
     const response = await fetch(`/api/posts/${id}`, {
-        method: 'PUT',
-        body: JSON.stringify({ 
-            title,
-            content
-        }),
-        headers: { 'Content-Type': 'application/json' }
+      method: 'PUT',
+      body: JSON.stringify({
+        title,
+        post_text
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
     });
-
+  
     if (response.ok) {
-        document.location.replace('/dashboard');
+      document.location.replace('/dashboard/');
     } else {
-        alert(response.statusText);
-    };
-};
-
-document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
+      alert(response.statusText);
+    }
+  }
+  
+  document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);

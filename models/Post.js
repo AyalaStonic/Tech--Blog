59e68499a8399c974c96eb11c67ca092ece49de9
AyalaStonic <ayaladuvan876@gmail.com>
2022-10-24 16:04:20 -0400

    Removed email requirements

--- models/Post.js
+++ models/Post.js
@@ -1,39 +1,46 @@
-const { Model, DataTypes } = require("sequelize");
-const sequelize = require("../config/connection");
-// create our Post model
+const {
+    Model,
+    DataTypes
+} = require('sequelize');
+const sequelize = require('../config/connection');
+
+
 class Post extends Model {}
 
-// create fields/columns for Post model
-Post.init(
-  {
+Post.init({
     id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
     },
     title: {
-      type: DataTypes.STRING,
-      allowNull: false,
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            len: [1]
+        }
     },
-    post_text: {
-      type: DataTypes.STRING,
-      allowNull: false
+    content: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            len: [1]
+        }
     },
     user_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: "user",
-        key: "id",
-      },
-    },
-  },
-  {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'user',
+            key: 'id'
+        }
+    }
+}, {
     sequelize,
     freezeTableName: true,
     underscored: true,
-    modelName: "post",
-  }
-);
+    modelName: 'post'
+})
+
 
 module.exports = Post;
